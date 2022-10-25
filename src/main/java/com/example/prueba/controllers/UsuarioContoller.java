bcb788ca84fd408779603abf84a84d240a2e77c2
Yorman Valencia <yormanvale@outlook.com>
2022-10-24 23:26:16 -0500

    Proyecto V-1

---
+++ src/main/java/com/example/prueba/controllers/UsuarioContoller.java
@@ -0,0 +1,48 @@
+package com.example.prueba.controllers;
+
+import com.example.prueba.models.UsuarioModel;
+import com.example.prueba.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioContoller {
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping()
+    public ArrayList<UsuarioModel> obtenerUsuarios(){
+        return usuarioService.obtenerUsuarios();
+    }
+
+    @PostMapping()
+    public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuarioModel){
+        return this.usuarioService.guardarUsuario(usuarioModel);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<UsuarioModel> obtenerUsuarioPorId(@PathVariable("id") Long id){
+        return this.usuarioService.obtenerPorId(id);
+    }
+
+    @GetMapping("/query")
+    public ArrayList<UsuarioModel> obtenerUsuarioPorPrioridad(@RequestParam("prioridad") Integer prioridad){
+        return this.usuarioService.obtenerPorPrioridad(prioridad);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String eliminarPorId(@PathVariable("id") Long id){
+        boolean ok = this.usuarioService.eliminarUsuario(id);
+        if (ok){
+            return "Se elimino el usuario con id" +id;
+        }else {
+            return "No se pudo eliminar el usuario con id" +id;
+        }
+    }
+
+
+}
---
+++ src/main/java/com/example/prueba/models/UsuarioModel.java
@@ -0,0 +1,48 @@
+package com.example.prueba.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuario")
+public class UsuarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+    private String name;
+    private  String email;
+    private  Integer prioridad;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPrioridad(Integer prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Integer getPrioridad() {
+        return prioridad;
+    }
+}
